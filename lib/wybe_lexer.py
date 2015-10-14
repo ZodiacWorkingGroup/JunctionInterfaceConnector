2e1e1d747f8a740f7eb5470fc22d9043c7c72974
Nathan <nathantronnes@gmail.com>
2015-10-14 11:05:21 -0800

    Did more lexery stuff
    New syntax: \ escaped names, etc.

--- lib/wybe_lexer.py
+++ lib/wybe_lexer.py
@@ -3,7 +3,9 @@ from UtopiaLexer import lexer
 STRING = 'str'
 INDEX = 'ind'
 NAME = 'name'
+RAWNAME = 'rn'
 CALL = 'call'
+SHORTCALL = 'scall'
 BRACK = 'bracket'
 SEP = 'separator'
 TO = 'to'
@@ -16,10 +18,12 @@ def lex(script):
     lexr.add_token_expr(r'^[\s]', None)
     lexr.add_token_expr(r'^\[[^\]]*\]', INDEX, (1, -1, 1))
     lexr.add_token_expr(r'[[{\]}]', BRACK)
-    lexr.add_token_expr(r',', SEP)
-    lexr.add_token_expr(r':', TO)
-    lexr.add_token_expr(r'^`', CALL, (1, None, 1))
-    lexr.add_token_expr(r'^[^\s]*', NAME)
+    # lexr.add_token_expr(r',', SEP)
+    # lexr.add_token_expr(r':', TO)
+    lexr.add_token_expr(r'^[a-zA-Z_][a-zA-Z0-9_]*', NAME)
+    lexr.add_token_expr(r'^\\[a-zA-Z_][a-zA-Z0-9_]*', RAWNAME, (1, None, 1))
+    lexr.add_token_expr(r'^`[a-zA-Z_][a-zA-Z0-9_]*', SHORTCALL, (1, None, 1))
+    lexr.add_token_expr(r'^[\s]*', CALL)
 
     return lexr.lex(script)
 
